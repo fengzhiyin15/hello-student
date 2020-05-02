a53ef47391dc1ccc2a91a96297902eaf4287234f
cuiyingpu <317835120@qq.com>
2020-05-02 16:38:07 +0800

    test for git

--- aa.py
+++ aa.py
@@ -5,4 +5,5 @@
 import numpy as np
 
 a = np.array([1, 2, 3])
+a = np.array([1, 2, 3])
 
--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 #! /user/bin/env python
 # -*- coding: utf-8 -*-
-# author: cyp time: 2020/5/2 
+# author: cyp time: 2020/5/2
+
+import numpy as np
+
+a = np.array([1, 2, 3])
